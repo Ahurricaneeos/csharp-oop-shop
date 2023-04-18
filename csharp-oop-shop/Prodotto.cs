32344719b2fdeb8835fa3d877599b487e56af4d2
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-18 17:30:29 +0200

    Added class and method

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -1,12 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace csharp_oop_shop
 {
     public class Prodotto
     {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private int iva;
+
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            int codice = new Random().Next(0, 99999999);
+            this.codice = codice;
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        public int GetCodice()
+        {
+            return codice;
+        }
+        public int GetIva()
+        {
+            return iva;
+        }
+        public void SetIva(int iva)
+        {
+            this.iva = iva;
+        }
+        public string GetNome()
+        {
+            return nome;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string GetDescrizione()
+        {
+            return descrizione;
+        }
+        public void SetDescrizione(string descrizione)
+        {
+            this.descrizione = descrizione;
+        }
+        public double GetPrezzo()
+        {
+            return prezzo;
+        }
+        public void SetPrezzo(double prezzo)
+        {
+            this.prezzo = prezzo;
+        }
+        private string PrezzoPlusIva()
+        {
+            double ivaSulPrezzo = prezzo + (prezzo * iva / 100);
+            string Iva = ivaSulPrezzo.ToString();
+            return Iva;
+        }
+
+        private string NomeEsteso()
+        {
+            string nomePiuCodice = codice + "-" + nome;
+            return nomePiuCodice;
+        }
+
+        public void StampaProdottiSenzaIva()
+        {
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome prodotto: " + nome);
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("Prezzo: " + prezzo + "€");
+        }
+
+        public void StampaProdottiConIva()
+        {
+            Console.WriteLine("Codice: " + codice);
+            Console.WriteLine("Nome prodotto: " + nome);
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("IVA: " + iva + "%");
+            Console.WriteLine("Prezzo con iva: " + PrezzoPlusIva() + "€");
+        }
+
+        public void StampaProdottiConcatenati()
+        {
+            Console.WriteLine("Codice e nome del prodotto: " + NomeEsteso());
+            Console.WriteLine("Descrizione prodotto: " + descrizione);
+            Console.WriteLine("IVA: " + iva + "%");
+            Console.WriteLine("Prezzo con iva: " + PrezzoPlusIva() + "€");
+        }
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using csharp_oop_shop;
+
+Console.WriteLine("Inserisci IVA: ");
+int inputIva = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci nome prodotto: ");
+string nomeProdotto = Console.ReadLine();
+
+Console.WriteLine("Inserisci descrizione prodotto: ");
+string descrizioneProdotto = Console.ReadLine();
+
+Console.WriteLine("Inserisci prezzo prodotto: ");
+double prezzoProdotto = double.Parse(Console.ReadLine());
+
+
+Prodotto primoProdotto = new Prodotto(nomeProdotto, descrizioneProdotto, prezzoProdotto, inputIva);
+
+Console.WriteLine("- - PRODOTTO N.1 - -");
+Console.WriteLine();
+primoProdotto.StampaProdottiSenzaIva();
+Console.WriteLine(" ");
+primoProdotto.StampaProdottiConIva();
+Console.WriteLine(" ");
+primoProdotto.StampaProdottiConcatenati();
+Console.WriteLine();
